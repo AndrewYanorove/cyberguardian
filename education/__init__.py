75d8f3377185318aa8495ec35d39741974e72ab4
AndrewYanorove <andrejanorov687@gmail.com>
2025-10-08 22:25:29 +0300

    изменения в модуле обучения

--- education/__init__.py
+++ education/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 
-education_bp = Blueprint('education', __name__)
+education_bp = Blueprint('education', __name__, template_folder='templates')
 
-# Импорты должны быть после создания Blueprint
 from . import routes
